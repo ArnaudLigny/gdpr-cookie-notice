efa546dd5304f669057ca23e79d1f5785e374610
Arnaud Ligny <arnaud@ligny.org>
2018-09-07 17:41:31 +0200

    Enhance
    - new: disable categories
    - new: FR lang
    - update: primary color
    - clean code / fix bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,99 +1,114 @@
-var path = require('path'),
-    gulp = require('gulp'),
-    watch = require('gulp-watch'),
-    sourcemaps = require('gulp-sourcemaps'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    notify = require('gulp-notify'),
-    plumber = require('gulp-plumber'),
-    sass = require('gulp-sass'),
-    rename = require("gulp-rename"),
-    browserify = require('gulp-browserify'),
-    autoprefixer = require('gulp-autoprefixer');
-    http = require('http');
-    ecstatic = require('ecstatic');
-    htmlToJs = require('gulp-html-to-js');
-    html2js = require('gulp-html2js');
-    eslint = require('gulp-eslint');
+var path = require("path");
+var gulp = require("gulp");
+var watch = require("gulp-watch");
+var sourcemaps = require("gulp-sourcemaps");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var notify = require("gulp-notify");
+var plumber = require("gulp-plumber");
+var sass = require("gulp-sass");
+var rename = require("gulp-rename");
+var browserify = require("gulp-browserify");
+var autoprefixer = require("gulp-autoprefixer");
+var http = require("http");
+var ecstatic = require("ecstatic");
+var htmlToJs = require("gulp-html-to-js");
+var html2js = require("gulp-html2js");
+var eslint = require("gulp-eslint");
+var util = require("gulp-util");
 
 var config = {
-    javascript: {
-        path: {
-            src: path.join('src/js'),
-            dist: path.join('dist')
-        }
-    },
-    sass: {
-        path: {
-            src: path.join('src/sass'),
-            dist: path.join('dist')
-        }
+  javascript: {
+    path: {
+      src: path.join("src/js"),
+      dist: path.join("dist")
     }
+  },
+  sass: {
+    path: {
+      src: path.join("src/sass"),
+      dist: path.join("dist")
+    }
+  }
 };
 
-var onJSError = function (err) {
-    notify({
-        message: err
-    });
+var onJSError = function(err) {
+  notify({
+    message: err
+  });
 };
 
-gulp.task('styles:sass', function () {
-
-    return gulp.src(path.join(config.sass.path.src, '**', '*.scss'))
-        .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest(config.sass.path.dist));
+gulp.task("styles:sass", function() {
+  return gulp
+    .src(path.join(config.sass.path.src, "**", "*.scss"))
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest(config.sass.path.dist));
 });
 
-gulp.task('javascript', function () {
-    return gulp.src(['node_modules/js-cookie/src/js.cookie.js', 'src/js/templates.js','src/js/script.js', 'src/langs/en.js',])
+gulp.task("javascript", function() {
+  return gulp
+    .src([
+      "node_modules/js-cookie/src/js.cookie.js",
+      "src/js/templates.js",
+      "src/js/script.js",
+      "src/langs/*.js"
+    ])
     .pipe(sourcemaps.init())
-    .pipe(concat('script.js'))
+    .pipe(concat("script.js"))
     .pipe(uglify())
     .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest(config.javascript.path.dist))
+    .pipe(gulp.dest(config.javascript.path.dist));
 });
 
-gulp.task("watch:sass", function () {
-    var paths = path.join(config.sass.path.src, '**', '*.scss');
-    return gulp.watch(paths, function () {
-        return gulp.start('styles:sass');
-    }, {read: false})
+gulp.task("watch:sass", function() {
+  var paths = path.join(config.sass.path.src, "**", "*.scss");
+  return gulp.watch(
+    paths,
+    function() {
+      return gulp.start("styles:sass");
+    },
+    { read: false }
+  );
 });
 
-gulp.task("watch:javascript", function () {
-    return watch(path.join(config.javascript.path.src, '**', '*.js'), function () {
-        return gulp.start('javascript');
-    }, {read: false});
+gulp.task("watch:javascript", function() {
+  return watch(
+    path.join(config.javascript.path.src, "**", "*.js"),
+    function() {
+      return gulp.start("javascript");
+    },
+    { read: false }
+  );
 });
 
-gulp.task('views:compile', function () {
-    gulp.src('src/html/*.html')
-    .pipe(html2js('templates.js', {
-      adapter: 'javascript',
-      base: 'src/html',
-      name: 'gdpr-cookie-notice-templates'
-    }))
-    .pipe(gulp.dest('src/js/'));
+gulp.task("views:compile", function() {
+  gulp
+    .src("src/html/*.html")
+    .pipe(
+      html2js("templates.js", {
+        adapter: "javascript",
+        base: "src/html",
+        name: "gdpr-cookie-notice-templates"
+      })
+    )
+    .pipe(gulp.dest("src/js/"));
 });
 
-gulp.task('lint', () => {
-    return gulp.src(['src/js/script.js'])
+gulp.task("lint", () => {
+  return gulp
+    .src(["src/js/script.js"])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('default', ['lint'], function () {
-    http.createServer(
-      ecstatic({ root: __dirname })
-    ).listen(3000);
-
-    console.log('Listening on :3000');
-
-    gulp.start('watch:sass');
-    gulp.start('views:compile');
-    gulp.start('watch:javascript');
+gulp.task("default", ["lint"], function() {
+  http.createServer(ecstatic({ root: __dirname })).listen(3000);
+  util.log("Server is runnig: http://localhost:3000");
+  gulp.start("watch:sass");
+  gulp.start("views:compile");
+  gulp.start("watch:javascript");
 });
---
+++ src/langs/fr.js
@@ -0,0 +1,22 @@
+//Add strings
+gdprCookieNoticeLocales.fr = {
+  description:
+    "Nous utilisons des cookies pour vous offrir une meilleure expérience de navigation, personnaliser le contenu et les annonces, fournir des fonctionnalités de médias sociaux et analyser notre trafic. Lisez comment nous utilisons les cookies et comment vous pouvez les contrôler en cliquant sur Réglage des cookies. Vous consentez à nos cookies si vous continuez à utiliser ce site.",
+  settings: "Paramètres des cookies",
+  accept: "Accepter",
+  statement: "Conditions d'utilisations",
+  save: "Enregitsrer",
+  always_on: "Toujours actif",
+  cookie_essential_title: "Cookies indispensables au site web",
+  cookie_essential_desc:
+    "Les cookies nécessaires permttant de rendre un site Web utilisable en activant des fonctions de base telles que la navigation au sein des pages et l'accès à des zones sécurisées du site Web. Le site Web ne peut pas fonctionner correctement sans ces cookies.",
+  cookie_performance_title: "Cookies de performance",
+  cookie_performance_desc:
+    "Ces cookies sont utilisés pour améliorer les performances et les fonctionnalités des sites Web, mais ils ne sont pas indispensables à leur utilisation. Par exemple, il stocke votre langue préférée ou la région dans laquelle vous vous trouvez.",
+  cookie_analytics_title: "Cookies de statistiques",
+  cookie_analytics_desc:
+    "Nous utilisons des cookies d'analyse pour nous aider à mesurer la manière dont les utilisateurs interagissent avec le contenu du site Web, ce qui nous aide à personnaliser nos sites Web et notre application afin d'améliorer votre expérience.",
+  cookie_marketing_title: "Cookies marketing",
+  cookie_marketing_desc:
+    "Ces cookies sont utilisés pour rendre les messages publicitaires plus pertinents pour vous et pour vos centres d'intérêt. L'objectifs est d'afficher des publicités pertinentes et engageantes pour l'utilisateur individuel et, par conséquent, plus utiles pour les éditeurs et les annonceurs tiers."
+};
